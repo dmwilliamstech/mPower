5b15eeee652f978c8ccf2e85909bb6de584debe5
David Williams <dmwilliams.tech@gmail.com>
2015-08-06 09:08:22 -0500

    changed asset.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += ['style2Demo.css', 'reset.css', 
-	'fonts.css', 'intro.js']
+#Rails.application.config.assets.precompile += ['style2Demo.css', 'reset.css', 'fonts.css', 'intro.js']
