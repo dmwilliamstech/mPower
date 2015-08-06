65e2773f358f3ba46987975ee4359f303157dfff
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-08-06 09:51:01 -0400

    fixed the intro just save

--- app/assets/javascripts/intro.js
+++ app/assets/javascripts/intro.js
@@ -2,8 +2,8 @@ var timer = 0; //setting this up as global
 
 $(document).ready( function () {
 
-        $("#mySite").hide(); // you could have the site as display:none in css instead of this line.
-        timer = setInterval( showSite , 18000 ) // here's your 2 seconds delay
+       // $("#mySite").hide(); // you could have the site as display:none in css instead of this line.
+        timer = setInterval( showSite , 8000 ) // here's your 2 seconds delay
 
 });
 
