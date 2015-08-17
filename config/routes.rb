2b8f36d062cac22d4792d1f9d844e4923767829f
David Williams <dmwilliams.tech@gmail.com>
2015-08-17 11:40:37 -0500

    changed color of homepage and mpwr

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
   root "pages#home"
 #  root "posts#index"
 
-resources "contacts", only: [:new, :create]
+# resources "contacts", only: [:new, :create]
 
-  get '/contact', to: 'pages#contact'
-  get '/about', to: 'pages#about'
-  get '/whoweare', to: 'pages#whoweare'
-  get '/aboutus', to: 'pages#aboutus'
-  get '/whatwedo', to: 'pages#whatwedo'
+#   get '/contact', to: 'pages#contact'
+#   get '/about', to: 'pages#about'
+#   get '/whoweare', to: 'pages#whoweare'
+#   get '/aboutus', to: 'pages#aboutus'
+#   get '/whatwedo', to: 'pages#whatwedo'
   get '/mpwr', to: 'pages#mpower'
   get "*path" => redirect("/mpwr")
 
