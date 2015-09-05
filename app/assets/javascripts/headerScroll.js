0e2a7c61a4740fa301b51bf7aa318dbc940483d7
David Williams <dmwilliams.tech@gmail.com>
2015-09-05 16:06:31 -0500

    changed logo

--- app/assets/javascripts/headerScroll.js
+++ app/assets/javascripts/headerScroll.js
@@ -5,9 +5,7 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('#mainNav').css('background-color', 'rgba(34,34,34,0.9)');
-       } else {
-          $('#mainNav').css('background-color', 'transparent');
-       }
+          $('#mainNav').css('background-color', 'rgba(20,20,20,.9)');
+       } 
    });
 });
