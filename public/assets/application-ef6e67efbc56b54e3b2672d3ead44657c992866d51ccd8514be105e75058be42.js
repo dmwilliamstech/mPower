5f68ecbf23a15af2e2c0932df00be2c4c103e51a
dmwilliamstech <dmwilliams.tech@gmail.com>
2015-09-05 17:12:50 -0400

    clobbered assets and raked

--- public/assets/application-ef6e67efbc56b54e3b2672d3ead44657c992866d51ccd8514be105e75058be42.js
+++ public/assets/application-ef6e67efbc56b54e3b2672d3ead44657c992866d51ccd8514be105e75058be42.js
@@ -10059,10 +10059,8 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('#mainNav').css('background-color', 'rgba(34,34,34,0.9)');
-       } else {
-          $('#mainNav').css('background-color', 'transparent');
-       }
+          $('#mainNav').css('background-color', 'rgba(20,20,20,.9)');
+       } 
    });
 });
 var timer = 0; //setting this up as global
